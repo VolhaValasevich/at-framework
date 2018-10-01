6b5dbbd03f19a51e84f11b0ce8ada969f4a888fa
Volha Valasevich <Volha_Valasevich@epam.com>
2018-10-01 15:48:22 +0300

    add somePOs and PO collector

---
+++ test/e2e/step-definitions/util/POCollector.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const fs = require('fs');
+
+class POCollector {
+    constructor() {
+        this.masterPO = {};
+    }
+
+    getReferences(obj) {
+        if (obj.ref !== undefined) {
+            const ref = require(path.resolve(obj.ref));
+            obj = ref;
+        }
+        if (typeof obj === 'object') {
+            Object.keys(obj).forEach((key) => {
+                obj[key] = this.getReferences(obj[key]);
+            });
+        }
+        return obj;
+    }
+
+    getAllPages(dir) {
+        const pages = fs.readdirSync(path.resolve(dir));
+        pages.forEach((page) => {
+            let pageObj = require(path.resolve(dir, page));
+            pageObj = this.getReferences(pageObj);
+            this.masterPO[pageObj.name] = pageObj;
+        })
+    }
+
+    saveMasterObject(dir) {
+        fs.writeFileSync(path.resolve(dir, 'MasterPO.json'), JSON.stringify(this.masterPO));
+    }
+
+    collectData(dir) {
+        this.getAllPages(dir);
+        this.saveMasterObject(dir);
+    }
+}
+
+module.exports = new POCollector();
+
