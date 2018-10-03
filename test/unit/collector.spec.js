afc3e0f11d132026c25cd1f1b6ca8784d99dc964
Volha Valasevich <Volha_Valasevich@epam.com>
2018-10-03 12:47:46 +0300

    move unit tests folder

--- test/unit/collector.spec.js
+++ test/unit/collector.spec.js
@@ -1,12 +1,12 @@
 'use strict'
 const chai =  require('chai');
 const expect = chai.expect;
-const collector = require('../test/e2e/step-definitions/util/POCollector');
+const collector = require('../e2e/step-definitions/util/POCollector');
 const expectedResult = require('./resources/expectedMasterPO.json')
 
 describe('Unit tests for Page Object Collector', () => {
     it('should collect jsons into a master file', () => {
-        collector.collectData('./dev/resources/');
+        collector.collectData('./test/unit/resources/');
         expect(require('./resources/MasterPO.json')).to.eql(expectedResult);
     })
 })
