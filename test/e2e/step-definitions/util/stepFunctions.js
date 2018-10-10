b0c637e45f61f8675ae1f1c98ea8dc9511d3b4d7
Volha Valasevich <Volha_Valasevich@epam.com>
2018-10-10 15:44:49 +0300

    add error handling to stepfunctions + add & rewrite unit tests

--- test/e2e/step-definitions/util/stepFunctions.js
+++ test/e2e/step-definitions/util/stepFunctions.js
@@ -9,7 +9,10 @@ class StepFunctions {
 
     isElementPresent(alias) {
         return this.helper.getElement(alias).then((el) => {
+            if (el.length) throw new Error('element is a collection');
             return el.isPresent();
+        }).catch((err) => {
+            throw new Error(`Cannot check if [${alias}] is present - ${err.message}`);
         })
     }
 
@@ -35,7 +38,7 @@ class StepFunctions {
                 expectedConditionFunction = EC.stalenessOf.bind(EC);
                 break;
             default:
-                throw new Error('Wrong expected condition provided.');
+                throw new Error(`[${shouldBe}] is not an expected condition`);
         }
         return expectedConditionFunction;
     }
@@ -44,41 +47,57 @@ class StepFunctions {
         const expectedConditionFunction = this.expectedCondition(shouldBe);
         return this.helper.getElement(alias).then((el) => {
             return browser.wait(expectedConditionFunction(el), 30000);
-        })
+        }).catch((err) => {
+            throw new Error(`Cannot wait until [${alias}] is ${shouldBe} - ${err.message}`);
+        }) 
     }
 
     sendKeys(alias, keys) {
         return this.helper.getElement(alias).then((el) => {
+            if (el.length) throw new Error('element is a collection');
             return el.sendKeys(keys);
-        }) 
+        }).catch((err) => {
+            throw new Error(`Cannot send keys to [${alias}] - ${err.message}`);
+        })  
     }
 
     getText(alias) {
         return this.helper.getElement(alias).then((el) => {
+            if (el.length) throw new Error('element is a collection');
             return el.getText();
-        }) 
+        }).catch((err) => {
+            throw new Error(`Cannot get text of [${alias}] - ${err.message}`);
+        })  
     }
 
     click(alias) {
         return this.helper.getElement(alias).then((el) => {
+            if (el.length) throw new Error('element is a collection');
             return el.click();
+        }).catch((err) => {
+            throw new Error(`Cannot click on [${alias}] - ${err.message}`);
         }) 
     }
 
     getNumberOfElements(alias) {
         return this.helper.getElement(alias).then((el) => {
+            if (!el.length) throw new Error('element is not a collection');
             return el.length;
-        })
+        }).catch((err) => {
+            throw new Error(`Cannot get number of [${alias}] - ${err.message}`);
+        }) 
     }
 
     getElementFromCollectionByText(alias, text) {
         return this.helper.getElement(alias).then(async(collection) => {
+            if (!collection.length) throw new Error(`Cannot get element with text [${text}] - [${alias}] is not a collection!`);
             for (let i = 0; i < collection.length; i++) {
                 const elementtext = await collection[i].getText();
                 if (text === elementtext) {
                     return collection[i];
                 }
             }
+            throw new Error(`No element with text [${text}] in [${alias}]!`)
         })
     }
 }
