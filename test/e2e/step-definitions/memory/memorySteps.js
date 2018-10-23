b29f1873a1d69d047021e189e98bfb025fdbe3e0
Volha Valasevich <Volha_Valasevich@epam.com>
2018-10-23 17:04:53 +0300

    add awaits to steps

--- test/e2e/step-definitions/memory/memorySteps.js
+++ test/e2e/step-definitions/memory/memorySteps.js
@@ -7,23 +7,23 @@ const logger = require('../util/logger').logger;
 When(/^I remember text of "([^"]*)" as "([^"]*)"/, async(alias, key) => {
     logger.info(`I remember text of [${alias}] as [${key}]`);
     const text = await step.getText(alias);
-    memory.store(key, text);
+    await memory.store(key, text);
 })
 
 When(/^I remember number of "([^"]*)" as "([^"]*)"/, async(alias, key) => {
     logger.info(`I remember number of [${alias}] as [${key}]`);
     const number = await step.getNumberOfElements(alias);
-    memory.store(key, number);
+    await memory.store(key, number);
 })
 
 When(/^I remember page title as "([^"]*)"/, async(key) => {
     logger.info(`I remember page title as [${key}]`);
     const text = await browser.getTitle();
-    memory.store(key, text);
+    await memory.store(key, text);
 })
 
 When(/^I remember attribute "([^"]*)" of "([^"]*)" as "([^"]*)"/, async(attributeName, alias, key) => {
     logger.info(`I remember attribute [${attributeName}] of [${alias}] as [${key}]`);
     const attribute = await step.getAttribute(alias, attributeName);
-    memory.store(key, attribute);
+    await memory.store(key, attribute);
 })
