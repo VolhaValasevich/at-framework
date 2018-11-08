a6f316880cac63defaadd8d5146f220a5ff942e2
Volha Valasevich <Volha_Valasevich@epam.com>
2018-11-08 10:36:27 +0300

    fix waiter step

--- test/e2e/step-definitions/actions/waiters.js
+++ test/e2e/step-definitions/actions/waiters.js
@@ -1,10 +1,10 @@
 'use strict';
-let {Then, When, Given} = require('cucumber');
+let { When } = require('cucumber');
 const step = require('../util/stepFunctions');
 const memory = browser.params.MEMORY;
 const logger = require('../util/logger').logger;
 
-When(/^I wait until "([^"]*)" is (.*)$/, async (alias, shouldBe) => { // (present|clickable|visible|invisible|selected|gone)
+When(/^I wait until "([^"]*)" is (.*)$/, async (alias, shouldBe) => { 
     alias = await memory.parseString(alias);
     shouldBe = await memory.parseString(shouldBe);
     logger.info(`I wait until [${alias}] is ${shouldBe}`);
